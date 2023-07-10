f6b04b0c2f045c1b6145e4882f1647795605e485
Hitarth-Vyas <82762525+Hitarth-Vyas@users.noreply.github.com>
2023-07-10 12:28:30 +0530

    Update web-request.service.ts

--- src/app/services/web-request.service.ts
+++ src/app/services/web-request.service.ts
@@ -8,7 +8,7 @@ export class WebRequestService {
   readonly ROOT_URL: any;
 
   constructor(private http: HttpClient) {
-    this.ROOT_URL = 'http://localhost:5000';
+    this.ROOT_URL = 'https://backend-mean-edu.vercel.app';
   }
 
   get(uri: string) {
